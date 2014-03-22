a54cd43ffc17b9825532aaaaa5b6370bf9453696
timothymiko <timothymiko@gmail.com>
2014-03-22 13:04:55 -0400

    added status header with gps info and next stop information

--- app/src/main/java/com/bearbusdriver/MainFragment.java
+++ app/src/main/java/com/bearbusdriver/MainFragment.java
@@ -1,12 +1,15 @@
 package com.bearbusdriver;
 
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
@@ -32,10 +35,14 @@ public class MainFragment extends Fragment {
     private Bus bus;
 
     private View mRoot;
+    private ImageView statusDrawable;
 
+    private Handler mHandler;
     private Timer timer;
 
     private ParseQuery<ParseObject> query;
+
+    private boolean isTransmittingLocation;
 
     private static final int GPS_UPDATE_INTERVAL = 10000;
 
@@ -58,39 +65,47 @@ public class MainFragment extends Fragment {
         if (getArguments() != null)
             bus = ParseApplication.activeBusLines.get(getArguments().getInt(ARG_BUS_INDEX));
         gps = new GPSTracker(getActivity());
+        isTransmittingLocation = true;
+        mHandler = new Handler();
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
-        query = ParseQuery.getQuery("Bus");
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                if (gps.canGetLocation()) {
+        if (timer == null) {
+            timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    if (gps.canGetLocation()) {
 
-                    final double latitude = gps.getLatitude();
-                    final double longitude = gps.getLongitude();
+                        final double latitude = gps.getLatitude();
+                        final double longitude = gps.getLongitude();
 
-                    bus.latitude = gps.getLatitude();
-                    bus.longitude = gps.getLongitude();
+                        bus.latitude = gps.getLatitude();
+                        bus.longitude = gps.getLongitude();
 
-                    try {
-                        ParseObject object = query.get(bus.id);
-                        ParseGeoPoint geoPoint = new ParseGeoPoint(latitude, longitude);
-                        object.put("location", geoPoint);
-                        object.save();
-                    } catch (ParseException e) {
-                        e.printStackTrace();
+                        try {
+                            query = ParseQuery.getQuery("Bus");
+                            ParseObject object = query.get(bus.id);
+                            ParseGeoPoint geoPoint = new ParseGeoPoint(latitude, longitude);
+                            object.put("location", geoPoint);
+                            object.save();
+                            isTransmittingLocation = true;
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                            isTransmittingLocation = false;
+                        }
+
+                        updateStatusDrawable();
+                    } else {
+                        isTransmittingLocation = false;
+                        updateStatusDrawable();
                     }
-
-                } else {
-                    Log.w("updateGPSRunnable", "Error retrieving GPS coordinates");
                 }
-            }
-        }, 0, GPS_UPDATE_INTERVAL);
+            }, 0, GPS_UPDATE_INTERVAL);
+        }
 
     }
 
@@ -99,6 +114,9 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mRoot = inflater.inflate(R.layout.fragment_main, container, false);
+
+        statusDrawable = (ImageView) mRoot.findViewById(R.id.status_image);
+        updateStatusDrawable();
 
         return mRoot;
     }
@@ -109,4 +127,20 @@ public class MainFragment extends Fragment {
         timer.cancel();
         timer.purge();
     }
+
+    private void updateStatusDrawable() {
+        mHandler.post(updateStatusImage);
+    }
+
+    final Runnable updateStatusImage = new Runnable() {
+        @Override
+        public void run() {
+            Drawable drawable = getActivity().getResources().getDrawable(R.drawable.colored_circle);
+            if (isTransmittingLocation)
+                drawable.setColorFilter(0xff00ff66  , PorterDuff.Mode.SRC_ATOP);
+            else
+                drawable.setColorFilter(0xffcc0000, PorterDuff.Mode.SRC_ATOP);
+            statusDrawable.setImageDrawable(drawable);
+        }
+    };
 }
